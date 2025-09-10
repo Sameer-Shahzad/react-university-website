0a014111a6e1dcf8e6d6f0062deaae34b4bdc24d
Sameer-Shahzad <sameershahzad835@gmail.com>
2025-09-10 12:41:05 +0500

    eight commit

--- university-website/src/Components/About/About.jsx
+++ university-website/src/Components/About/About.jsx
@@ -14,9 +14,9 @@ const About = () => {
             <div className="w-full md:w-3/5 md:pl-20 text-center md:text-left">
                 <h3 className="text-md font-semibold text-blue-600 mb-2">ABOUT UNIVERSITY</h3>
                 <h1 className="text-4xl font-bold text-gray-800 mb-4">Nurturing Tomorrow's Leaders Today</h1>
-                <p className="text-gray-600 leading-relaxed mb-4">Embark on a transformative educational journey with our university's comprehensive education programs. Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education.</p>
-                <p className="text-gray-600 leading-relaxed mb-4">With a focus on innovation, hands-on learning, and personalized mentorship, our programs prepare aspiring educators to make a meaningful impact in classrooms, schools, and communities.</p>
-                <p className="text-gray-600 leading-relaxed">Whether you aspire to become a teacher, administrator, counselor, or educational leader, our diverse range of programs offers the perfect pathway to achieve your goals and unlock your full potential in shaping the future of education.</p>
+                <p className="text-gray-600 leading-relaxed mb-4 text-justify">Embark on a transformative educational journey with our university's comprehensive education programs. Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education.</p>
+                <p className="text-gray-600 leading-relaxed mb-4 text-justify">With a focus on innovation, hands-on learning, and personalized mentorship, our programs prepare aspiring educators to make a meaningful impact in classrooms, schools, and communities.</p>
+                <p className="text-gray-600 leading-relaxed text-justify">Whether you aspire to become a teacher, administrator, counselor, or educational leader, our diverse range of programs offers the perfect pathway to achieve your goals and unlock your full potential in shaping the future of education.</p>
             </div>
         </div>
     );
